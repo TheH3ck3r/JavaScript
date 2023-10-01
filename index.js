0f3c3a16d9351cc6297790e0dcf5a4e667c4b1bf
Михаил Нечаев <theh4ck3r@yandex.ru>
2023-10-01 12:00:38 +0300

    feat: complete task (7kyu)

--- index.js
+++
@@ -1,9 +0,0 @@
-// Write a function that returns the number of arguments it received.
-
-// args_count() --> 0
-// args_count('a') --> 1
-// args_count('a', 'b') --> 2
-
-const args_count = (...args) => {
-  return args.length;
-};

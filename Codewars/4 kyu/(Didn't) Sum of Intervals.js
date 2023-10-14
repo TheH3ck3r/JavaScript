352a3f357edebe49003a432eed9241a6e7e2cb10
Михаил Нечаев <theh4ck3r@yandex.ru>
2023-10-14 12:15:25 +0300

    feat: tried to complete task (4kyu)

--- Codewars/4 kyu/(Didn't) Sum of Intervals.js
+++ Codewars/4 kyu/(Didn't) Sum of Intervals.js
@@ -1,7 +1,7 @@
-// Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+// Write a function called sumIntervals/sum_intervals that accepts an sortedArray of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
 
 // Intervals
-// Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+// Intervals are represented by a pair of integers in the form of an sortedArray. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
 
 // Overlapping Intervals
 // List containing overlapping intervals:
@@ -43,23 +43,98 @@
 // Tests with large intervals
 // Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
 
+// V1
 // const sumIntervals = (ArrayOfIntervals) =>
 //   ArrayOfIntervals.reduce((result, interval) => {
 //     console.log("interval = ", interval[1] - interval[0]);
 //     return result + interval[1] - interval[0];
 //   }, 0);
 
-const sumIntervals = (ArrayOfIntervals) => {
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
-    (interval, i, sortedArray) => {
-      interval[1] > sortedArray[i + 1][0]
-        ? [interval[1], sortedArray[i + 1][0]]
-        : "";
-    }
-  );
+// V2
+// const sumIntervals = (ArrayOfIntervals) => {
+//   sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
+//     (interval, i, sortedArray) => {
+//       interval[1] > sortedArray[i + 1][0] && !sortedArray[i + 1][0]
+//         ? ""[interval[1], sortedArray[i + 1][0]]
+//         : "";
+//     }
+//   );
 
-  return "answer = ?";
+//   return "answer = ?";
+// };
+
+// V3
+// const sumIntervals = (ArrayOfIntervals) => {
+//   sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
+//     (interval, i, sortedArray) => {
+//       if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
+//         return `Новый интервал: ${[interval[0], sortedArray[i + 1][1]]}`;
+//       } else {
+//         return `Последний элемент: ${interval}`;
+//       }
+//     }
+//   );
+//   return sortedArray;
+// };
+
+// V4 (Работает, но не проходит часть тестов)
+const sumIntervals = (ArrayOfIntervals) => {
+  let mergedArray = [];
+  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (
+      i < sortedArray.length - 1 &&
+      sortedArray[i][1] > sortedArray[i + 1][0]
+    ) {
+      mergedArray.push([sortedArray[i][0], sortedArray[i + 1][1]]);
+      i++;
+    } else {
+      mergedArray.push(sortedArray[i]);
+    }
+  }
+
+  return mergedArray.reduce(
+    (result, interval) => result + interval[1] - interval[0],
+    0
+  );
 };
+
+// V4-1
+// const sumIntervals = (ArrayOfIntervals) => {
+//   let mergedArray = [];
+//   sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
+//     (interval, i, sortedArray) => {
+//       if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
+//         mergedArray.push([interval[0], sortedArray[i + 1][1]]);
+//         i++;
+//       } else {
+//         mergedArray.push(interval);
+//       }
+//     }
+//   );
+//   return mergedArray;
+// };
+
+// V5 (forEach нормально не работает)
+// const sumIntervals = (ArrayOfIntervals) => {
+//   let mergedArray = [];
+//   sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).forEach(
+//     (interval, i, sortedArray) => {
+//       if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
+//         mergedArray.push([interval[0], sortedArray[i + 1][1]]);
+//         i++;
+//       } else {
+//         mergedArray.push(interval);
+//       }
+//     }
+//   );
+
+//   return mergedArray.reduce(
+//     (result, interval) => result + interval[1] - interval[0],
+//     0
+//   );
+// };
 
 console.log(
   sumIntervals([

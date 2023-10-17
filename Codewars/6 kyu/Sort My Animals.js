981c5f428b029fc514581d8223f946ff74a56626
Михаил Нечаев <theh4ck3r@yandex.ru>
2023-10-17 17:41:30 +0300

    feat: complete (bad) task (6kyu)

---
+++ Codewars/6 kyu/Sort My Animals.js
@@ -0,0 +1,16 @@
+// Consider the following class:
+
+// var Animal = {
+//     name: "Cat",
+//     numberOfLegs: 4
+// }
+// Write a method that accepts a list of objects of type Animal, and returns a new list. The new list should be a copy of the original list, sorted first by the animal's number of legs, and then by its name.
+
+// If an empty list is passed in, it should return an empty list back.
+
+const sortAnimal = (animals) =>
+  animals
+    .sort((a, b) => a.numberOfLegs - b.numberOfLegs)
+    .sort((a, b) => {
+      if (a.numberOfLegs == b.numberOfLegs) return a.name.localeCompare(b.name);
+    });

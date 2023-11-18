326fbe5d1817caced98f20b82eccd03d3ea6ca5c
Михаил Нечаев <theh4ck3r@yandex.ru>
2023-11-18 10:32:05 +0300

    feat: complete task (4kyu)

--- Codewars/4 kyu/(Didn't) Sum of Intervals.js
+++ Codewars/4 kyu/(Didn't) Sum of Intervals.js
@@ -43,226 +43,41 @@
 // Tests with large intervals
 // Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
 
-// V1
-const sumIntervals_V1 = (ArrayOfIntervals) =>
-  ArrayOfIntervals.reduce((result, interval) => {
-    console.log("interval = ", interval[1] - interval[0]);
-    return result + interval[1] - interval[0];
-  }, 0);
-
-// V2
-const sumIntervals_V2 = (ArrayOfIntervals) => {
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
-    (interval, i, sortedArray) => {
-      interval[1] > sortedArray[i + 1][0] && !sortedArray[i + 1][0]
-        ? ""[(interval[1], sortedArray[i + 1][0])]
-        : "";
-    }
-  );
-
-  return "answer = ?";
-};
-
-// V3
-const sumIntervals_V3 = (ArrayOfIntervals) => {
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
-    (interval, i, sortedArray) => {
-      if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
-        return `Новый интервал: ${[interval[0], sortedArray[i + 1][1]]}`;
-      } else {
-        return `Последний элемент: ${interval}`;
-      }
-    }
-  );
-  return sortedArray;
-};
-
-// V4 (Работает, но не проходит часть тестов; Time: 866ms, Passed: 2, Failed: 3)
-const sumIntervals_V4 = (ArrayOfIntervals) => {
-  let mergedArray = [];
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]);
-
-  for (let i = 0; i < sortedArray.length; i++) {
-    if (
-      i < sortedArray.length - 1 &&
-      sortedArray[i][1] > sortedArray[i + 1][0]
-    ) {
-      mergedArray.push([sortedArray[i][0], sortedArray[i + 1][1]]);
-      i++;
-    } else {
-      mergedArray.push(sortedArray[i]);
-    }
-  }
-
-  return mergedArray.reduce(
-    (result, interval) => result + interval[1] - interval[0],
-    0
-  );
-};
-
-// V4-1
-const sumIntervals_V4_1 = (ArrayOfIntervals) => {
-  let mergedArray = [];
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).map(
-    (interval, i, sortedArray) => {
-      if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
-        mergedArray.push([interval[0], sortedArray[i + 1][1]]);
-        i++;
-      } else {
-        mergedArray.push(interval);
-      }
-    }
-  );
-  return mergedArray;
-};
-
-// V5 (forEach нормально не работает)
-const sumIntervals_V5 = (ArrayOfIntervals) => {
-  let mergedArray = [];
-  sortedArray = ArrayOfIntervals.sort((a, b) => a[0] - b[0]).forEach(
-    (interval, i, sortedArray) => {
-      if (i < sortedArray.length - 1 && interval[1] > sortedArray[i + 1][0]) {
-        mergedArray.push([interval[0], sortedArray[i + 1][1]]);
-        i++;
-      } else {
-        mergedArray.push(interval);
-      }
-    }
-  );
-
-  return mergedArray.reduce(
-    (result, interval) => result + interval[1] - interval[0],
-    0
-  );
-};
-
-// V6 Пока не работает :(
-const sumIntervals_V6 = (arrayOfIntervals) => {
-  arrayOfIntervals
-    .sort((a, b) => a[0] - b[0])
-    .filter((interval, i, arrayOfIntervals) => {
-      // console.log("interval[i + 1]: ", arrayOfIntervals[i + 1][0]);
-      // return interval[1] > interval[0] ? [interval[0], interval[1]] : interval;
-      return interval[1] > arrayOfIntervals[i + 1][0] &&
-        arrayOfIntervals[i + 1][1] != undefined
-        ? [interval[0], arrayOfIntervals[i + 1][1]]
-        : interval;
-    });
-
-  console.log("\nafter filter: ", arrayOfIntervals, "\n");
-
-  return arrayOfIntervals.reduce(
-    (result, interval) => result + interval[1] - interval[0],
-    0
-  );
-};
-
-// V7 Работает, но не проходит по времени: Execution Timed Out (12000 ms)
 const sumIntervals = (arrayOfIntervals) => {
-  arrayOfIntervals.sort((a, b) => a[0] - b[0]);
+  const mergedIntervals = [];
+  const sortedIntervals = [...arrayOfIntervals].sort((a, b) => a[0] - b[0]);
 
-  for (let i = 0; i < arrayOfIntervals.length; i++) {
-    // Если 2 интервал находиться в 1
+  for (const interval of sortedIntervals) {
+    // Если mergedIntervals пустой или текущий интервал не пересекается с последним в mergedIntervals
     if (
-      i < arrayOfIntervals.length - 1 &&
-      arrayOfIntervals[i][0] <= arrayOfIntervals[i + 1][0] &&
-      arrayOfIntervals[i][1] >= arrayOfIntervals[i + 1][1]
+      !mergedIntervals.length ||
+      interval[0] > mergedIntervals[mergedIntervals.length - 1][1]
     ) {
-      arrayOfIntervals.splice(i, 2, [
-        arrayOfIntervals[i][0],
-        arrayOfIntervals[i][1],
-      ]);
-      i--;
-    }
-    // Если 2 интервала пересекаются
-    else if (
-      i < arrayOfIntervals.length - 1 &&
-      arrayOfIntervals[i][1] >= arrayOfIntervals[i + 1][0]
-    ) {
-      arrayOfIntervals.splice(i, 2, [
-        arrayOfIntervals[i][0],
-        arrayOfIntervals[i + 1][1],
-      ]);
-      i--;
+      mergedIntervals.push(interval);
     } else {
-      continue;
+      // Интервалы пересекаются, обновляем последний интервал в mergedIntervals
+      mergedIntervals[mergedIntervals.length - 1][1] = Math.max(
+        mergedIntervals[mergedIntervals.length - 1][1],
+        interval[1]
+      );
     }
   }
-  return arrayOfIntervals.reduce(
+
+  return mergedIntervals.reduce(
     (result, interval) => result + interval[1] - interval[0],
     0
   );
 };
 
-console.log(
-  "\n Test 1: ",
-  sumIntervals([
-    [1, 4],
-    [7, 10],
-    [3, 5],
-  ]),
-  " | answer = 7\n",
-  "Test 2: ",
-  sumIntervals([
-    [0, 20],
-    [-100000000, 10],
-    [30, 40],
-  ]),
-  " | answer = 1e8 + 30\n",
-  "Random test 1: ",
-  sumIntervals([
-    [13, 19],
-    [16, 22],
-    [-19, -9],
-    [12, 19],
-  ]),
-  " | answer = 20\n",
-  "Random test 2: ",
-  sumIntervals([
-    [-6, -1],
-    [3, 5],
-    [6, 16],
-    [-14, -12],
-    [-2, 5],
-    [19, 23],
-    [2, 5],
-    [16, 24],
-    [10, 12],
-    [7, 12],
-  ]),
-  " | answer = 34\n",
-  "Random test 3: ",
-  sumIntervals([
-    [19, 22],
-    [-14, -4],
-    [-8, -7],
-    [-19, -12],
-  ]),
-  " | answer = 18\n",
-  "Random test 4: ",
-  sumIntervals([
-    [1, 4],
-    [3, 6],
-    [5, 8],
-    [7, 10],
-    [9, 12],
-  ]),
-  " | answer = 11\n",
-  "Random test 5: ",
-  sumIntervals([
-    [1, 20],
-    [2, 19],
-    [5, 15],
-    [8, 12],
-  ]),
-  " | answer = 19\n",
-  "Random test 6: ",
-  sumIntervals([
-    [2, 3],
-    [2, 6],
-    [2, 4],
-    [2, 9],
-    [2, 5],
-  ]),
-  " | answer = 7\n"
-);
+// Smart solution:
+// function sumIntervals(xs) {
+//   let ys = xs.sort(([a, b], [c, d]) => a - c);
+//   let m = -Number.MAX_VALUE;
+//   let res = 0;
+//   for (let [a, b] of ys) {
+//     m = Math.max(m, a);
+//     res += Math.max(0, b - m);
+//     m = Math.max(m, b);
+//   }
+//   return res;
+// }
